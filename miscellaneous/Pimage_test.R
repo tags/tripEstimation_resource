fe73c619a215ee54fcff4ecef92f2d26430f192b
mdsumner <mdsumner@gmail.com>
2013-06-07 06:42:14 +1000

    notes on what should be happening

--- miscellaneous/Pimage_test.R
+++ miscellaneous/Pimage_test.R
@@ -1,3 +1,25 @@
+## Pimage
+
+## creation is hidden, behind bin(), but Pimage() does that work
+##   x <- bin(chain)  ## this is enough, but grid argument to customize the grid details
+##   a Pimage is a list of child windows, and appears to the user as
+##   a 3D array of sorts, with x/y/time
+##   - none of the details need to be known to the user, though the Pimage object can be worked with in 
+##     the following ways
+##   raster(x, time.subset = length(x)) ## can be any index from 1:length(x)
+##   x[], x[1:length(x)]   # same as above
+##   x["weeks"] ## one of "days", "weeks", "months", "quarters", "years"
+##      this serves to break the Pimage time series into these durations based on the time index, and returns a
+##     3D raster object, with the formatted date-times as the layers names, or as metadata 
+
+##    the elements should always be trimmed internally, to avoid padding of 0
+##     raster also needs an option for trim() that takes a value
+
+
+## ??  should indexing prevent duplicates? warning
+
+
+
 library(raster)
 
 ##source("F:/GIT/tripEstimation_resource/miscellaneous/Pimage.R")
